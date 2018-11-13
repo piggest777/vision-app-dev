a1b32712fc6fa89e588660d3923bc8178831c71e
Денис Ракитин <piggest@mail.ru>
2018-11-13 06:50:38 +0300

    Initial Commit

---
+++ vision-app-dev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vision-app-dev
+//
+//  Created by Denis Rakitin on 2018-11-13.
+//  Copyright © 2018 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
